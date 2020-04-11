41e62f55a71616f2d9d877393629e973634e2d2b
riceflow <riceflow@gmail.com>
2020-04-11 22:14:25 +0900

    一旦完成形

---
+++ MySQL8/docker/mysql/initdb.d/01_schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE table1 (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(32) NOT NULL,
+    email VARCHAR(32) NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE table2 (
+    id INT NOT NULL AUTO_INCREMENT,
+    val VARCHAR(32) NOT NULL,
+    email VARCHAR(32) NOT NULL,
+    PRIMARY KEY (id)
+);
---
+++ MySQL8/docker/mysql/initdb.d/02_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO table1 (id,name,email) VALUES (1, 'a','xxxx@mail.co.jp');
+INSERT INTO table1 (id,name,email) VALUES (2, 'b','xxxx@mail.co.jp');
+INSERT INTO table1 (id,name,email) VALUES (3, 'c','xxxx@mail.co.jp');
+INSERT INTO table1 (id,name,email) VALUES (4, 'd','xxxx@mail.co.jp');
+INSERT INTO table2 (id,val,email) VALUES (1, 'A','xxxx@mail.co.jp');
+INSERT INTO table2 (id,val,email) VALUES (10, 'AA','xxxx@mail.co.jp');
